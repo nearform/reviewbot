26285de3151c4ad184de6ff86a3a5e6f50419261
Rafael Gonzaga <rafael.nunu@hotmail.com>
2023-04-19 13:36:11 -0300

    Adding an unnecessary refactor
    Surprise me, bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import webhookHandler from './functions/webhook/function.js'
 import createReviewHandler from './functions/createReview/function.js'
 
-export const webhook = webhookHandler
 export const createReview = createReviewHandler
+export const webhook = webhookHandler
+
