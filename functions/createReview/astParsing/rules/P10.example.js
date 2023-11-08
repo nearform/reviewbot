32a3ce2aaa1a4d8e45b3d9ee3fd5d9f53f31565c
Nicolas Herment <nherment@arkhaios.net>
2023-11-08 10:43:43 +0100

    feat: improve detection for AST rule P10 - await nested in a loop

--- functions/createReview/astParsing/rules/P10.example.js
+++ functions/createReview/astParsing/rules/P10.example.js
@@ -4,19 +4,54 @@ const myPromise = new Promise((resolve, reject) => {
   }, 300)
 })
 
-async function test1() {
+async function testFor() {
   for(let i = 0 ; i < 10 ; i++) {
     // expect:P10
     await myPromise()
   }
 }
 
-// function test2() {
-//   [1,2,3].map(async () => await myPromise())
-// }
+async function testForOf() {
+  const promises = [myPromise, myPromise]
+  for(let promise of promises) {
+    // expect:P10
+    await promise()
+  }
+}
 
-// function test3() {
+async function testForIn() {
+  const promises = [myPromise, myPromise]
+  for(let i in promises) {
+    // expect:P10
+    await promises[i]()
+  }
+}
 
-//   [1,2,3].map(async () => await myPromise())
+async function testWhile() {
+  let i = 0;
+  while(i < 10) {
+    // expect:P10
+    await myPromise()
+    i++
+  }
+}
 
-// }
+async function testDoWhile() {
+  let i = 0;
+  do {
+    // expect:P10
+    await myPromise()
+    i++
+  } while(i < 10)
+}
+
+function testArrayMap() {
+  // expect:P10
+  [1,2,3].map(async () => await myPromise())
+}
+
+async function testArrayForEach() {
+  const promises = [myPromise, myPromise]
+  // expect:P10
+  promises.forEach(async promise => await promise())
+}
